4509c4340039799e1b09ca645753fa29c9d325b8
gitkaviyarasi <kaviyarasikrishnannj@gmail.com>
2024-12-17 20:57:03 -0500

    corrected the errors and built the server folder

---
+++ server/src/routes/api/skills-route.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import {
+ getSkillById,
+  getUserById,
+  getSkillLevel,
+    getAllSkillsMap,
+} from '../../controller/skills-map-controller';
+
+const router = express.Router();
+
+// GET /skills/:id - Get a user by id
+router.get('/:id', getSkillById);
+
+// POST /users - Create a new user
+router.get('/id', getUserById);
+
+// PUT /users/:id - Update a user by id
+router.get('/:id', getSkillLevel);
+
+// DELETE /users/:id - Delete a user by id
+router.get('/:id', getAllSkillsMap);
+
+export { router as skillRouter };
